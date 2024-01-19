e8800e159c8fddd13d68f3ea6acf6eaf029b757e
spandith <sahil.pandith@niceactimize.com>
2024-01-19 13:48:23 +0530

    added some code to visualize Vue Lifecycle

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import VisualizeLifeCycle from '../components/VisualizeLifeCycle.vue'
+</script>
+
+<template>
+  <main>
+    <VisualizeLifeCycle msg="Visulize Life Cycle"/>
+  </main>
+</template>
