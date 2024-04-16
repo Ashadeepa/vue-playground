0755a5f32cd3f51b9fbd67494c7f85354eb1b519
Ashadeepa Debnath <ashadeepa.debnath@nice.com>
2024-04-16 17:56:35 +0530

    IPL Solution

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,31 +1,58 @@
 <template>
     <section class="cards">
-        <article class="card">
+        <article :key="match.id" v-for="match in matches" class="card">
             <picture class="thumbnail">
-                <img class="category__01" src="https://g.cricapi.com/iapi/261-637852957972423711.png?w=48" alt="" />
+                <img class="category__01" :src="winnerImage(match)" alt="" />
             </picture>
             <div class="card-content">
-                <p class="category category__01">T20</p>
-                <h2>RCB vs MI</h2>
-                <p>RCB won by 50 runs</p>
+                <p class="category category__01">{{match.matchType}}</p>
+                <h2>{{match.name}}</h2>
+                <p>{{match.status}}</p>
             </div>
             <footer>
             <div class="post-meta">
-                <span class="timestamp"><i class="fa fa-clock-o">06/04/2023 </i></span>
-                <span class="comments"><i class="fa fa-comments">Wankhede stadium, Mumbai</i></span>
+                <span class="timestamp"><i class="fa fa-clock-o"></i> {{getFormattedDate(new Date(match.date))}}</span>
+                <span class="comments"><i class="fa fa-comments"></i><a href="#"> {{match.venue}}</a></span>
                 </div>
             </footer>
         </article>
 </section>
-
+ 
 </template>
-
+ 
 <script setup>
-    
-
-
+    import {onMounted, ref, computed} from 'vue';
+    import axios from 'axios';
+ 
+    const matches = ref([]);
+ 
+    onMounted(async ()=> {
+        const response = await axios.get("currentMatches.json");
+        const {data} = response.data;
+        matches.value = data.matchList;
+    });
+ 
+    computed(()=>{
+ 
+    });
+ 
+    const getFormattedDate = (date)=> {
+     
+        return (date.getMonth() + 1) + '/'+
+        (date.getDate()) + '/'+
+        date.getFullYear()
+    }
+ 
+    const winnerImage = (winnerInfo)=> {
+        const winnerTeam = winnerInfo.status.split('won')[0].trim();
+        const teamInfo = winnerInfo.teamInfo.find((info)=> info.name === winnerTeam);
+        return teamInfo.img;
+    }
+ 
+ 
 </script>
-
+ 
 <style lang="scss">
-
+ 
 </style>
+ 
